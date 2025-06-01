15e542566e952c9f0eb46808faf99a90a60d0054
Lucas Le <lucas.lpnl@byteuptech.com>
2025-06-02 05:16:08 +0700

    Setup CICD

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
   output: 'export',
   distDir: 'out',
   images: {
     unoptimized: true,
   },
-  basePath: isProd ? '/ez-saga' : '',
 };
 
 module.exports = nextConfig;
